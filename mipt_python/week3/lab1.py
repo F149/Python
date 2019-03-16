8beb32c41ff201378fd715b6dacad30e5cea5599
sm1rnov <smirnov.avc@gmail.com>
2019-03-16 11:31:52 +0200

    Edit comments

--- mipt_python/week3/lab1.py
+++ mipt_python/week3/lab1.py
@@ -11,18 +11,17 @@ class FileReader:
         except OSError:
             return ""
 
-""""
-Mentors solutions
 
-class FileReader:
-    """Класс FileReader помогает читать из файла"""
-
-    def __init__(self, file_name):
-        self.file_name = file_name
-
-    def read(self):
-        try:
-            return open(self.file_name).read()
-        except IOError:
-            return ""
-""""
+#Mentors solutions
+#
+#class FileReader:
+#    """Класс FileReader помогает читать из файла"""
+#
+#    def __init__(self, file_name):
+#        self.file_name = file_name
+#
+#    def read(self):
+#        try:
+#            return open(self.file_name).read()
+#        except IOError:
+#            return ""
